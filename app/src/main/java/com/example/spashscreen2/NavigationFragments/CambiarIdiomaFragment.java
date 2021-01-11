4f7d5116798e0a9f829098d298245d7f79ef5870
rogerroque <reroqueo01@elpuig.xeill.net>
2021-01-11 18:41:39 +0100

    ff

--- app/src/main/java/com/example/spashscreen2/NavigationFragments/CambiarIdiomaFragment.java
+++ app/src/main/java/com/example/spashscreen2/NavigationFragments/CambiarIdiomaFragment.java
@@ -1,21 +1,16 @@
 package com.example.spashscreen2.NavigationFragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.example.spashscreen2.R;
 import com.example.spashscreen2.databinding.FragmentCambiarIdiomaBinding;
-import com.example.spashscreen2.databinding.FragmentPuntuacionesBinding;
 import com.squareup.picasso.Picasso;
-
 
 public class CambiarIdiomaFragment extends Fragment {
 
@@ -36,14 +31,14 @@ public class CambiarIdiomaFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
 
-        /*Picasso.get().load("https://i.ibb.co/9gh0sTC/spanish-1.png").into(binding.imgEspanol);
-        Picasso.get().load("https://i.ibb.co/x5jGmpf/english-1.png").into(binding.imgIngles);
-        Picasso.get().load("https://i.ibb.co/tczr5f2/german-1.png").into(binding.imgAleman);
-        Picasso.get().load("https://i.ibb.co/68mcTGt/french-2.png").into(binding.imgFrances);
-        Picasso.get().load("https://i.ibb.co/JCqZg5V/italian-1.png").into(binding.imgItaliano);
-        Picasso.get().load("https://i.ibb.co/hKx3mKb/korean-1.png").into(binding.imgCoreano);
-        Picasso.get().load("https://i.ibb.co/gZ3nP7c/chinee-1.png").into(binding.imgChino);
-        Picasso.get().load("https://i.ibb.co/54vpSB3/catalan-1.png").into(binding.imgCatalan);*/
+        Picasso.get().load("https://i.ibb.co/9gh0sTC/spanish-1.png").into(binding.imgspanish);
+        Picasso.get().load("https://i.ibb.co/x5jGmpf/english-1.png").into(binding.imgenglish);
+        Picasso.get().load("https://i.ibb.co/54vpSB3/catalan-1.png").into(binding.imgcatalan);
+        Picasso.get().load("https://i.ibb.co/tczr5f2/german-1.png").into(binding.imggerman);
+        Picasso.get().load("https://i.ibb.co/68mcTGt/french-2.png").into(binding.imgfrench);
+        Picasso.get().load("https://i.ibb.co/JCqZg5V/italian-1.png").into(binding.imgitalian);
+        Picasso.get().load("https://i.ibb.co/hKx3mKb/korean-1.png").into(binding.imgcorean);
+        Picasso.get().load("https://i.ibb.co/gZ3nP7c/chinee-1.png").into(binding.imgchinese);
 
     }
 }
