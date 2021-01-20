fcd28c4dc6824d31d927c7c54f8b7edc89d020f5
rogerroque <reroqueo01@elpuig.xeill.net>
2021-01-20 22:37:32 +0100

    Torre - 20/01/2021 - 22:37

--- app/src/main/java/com/example/spashscreen2/ResultadoPreguntasFragment.java
+++ app/src/main/java/com/example/spashscreen2/ResultadoPreguntasFragment.java
@@ -1,5 +1,7 @@
 package com.example.spashscreen2;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,33 +13,50 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.spashscreen2.databinding.FragmentResultadoPreguntasBinding;
+import com.github.jinatonic.confetti.CommonConfetti;
+
+import java.util.Random;
 
 public class ResultadoPreguntasFragment extends Fragment {
 
     private FragmentResultadoPreguntasBinding binding;
     private NavController navController;
+    private final Random random = new Random();
+    private int puntacion = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return (binding = FragmentResultadoPreguntasBinding.inflate(inflater, container, false)).getRoot();
+
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         navController = Navigation.findNavController(view);
 
-        binding.botonVolver.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navController.navigate(R.id.action_resultadoPreguntasFragment_to_homeFragment);
-            }
-        });
+        binding.botonVolver.setOnClickListener(v -> navController.navigate(R.id.action_resultadoPreguntasFragment_to_homeFragment));
+
+        puntacion = random.nextInt(10) + 1;
+        binding.resultado.setText(puntacion + "/10");
+
+        /*binding.confeti.setOnClickListener(v -> CommonConfetti.rainingConfetti((ViewGroup) view, new int[] { Color.YELLOW, Color.RED, Color.BLUE }).stream(3500));*/
+
+        if (puntacion >= 5) {
+            CommonConfetti.rainingConfetti((ViewGroup) view, new int[] {Color.YELLOW, Color.RED, Color.BLUE}).stream(3500);
+            Toast bien = Toast.makeText(requireContext(), R.string.muy_bien, Toast.LENGTH_LONG);
+            bien.show();
+        } else {
+            Toast mal = Toast.makeText(requireContext(), R.string.muy_mal, Toast.LENGTH_LONG);
+            mal.show();
+        }
 
     }
 }
